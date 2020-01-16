630e7f65f5e758ed719d9c4d1c494b7bb08c5028
Mohit Pandey <mohit.pandey@smartdatainc.net>
2020-01-16 10:54:49 +0530

    update main django app

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'openvino.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'openvino_api.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- openvino_api/urls.py
+++ openvino_api/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from openvino.views import StartApp
+from openvino_api.views import StartApp
 
 urlpatterns = [
     path('admin/', admin.site.urls),
